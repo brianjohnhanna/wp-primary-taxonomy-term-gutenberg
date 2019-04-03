9bffacc463a4aa3ad802d5a992fc2c9848f924cd
Brian John Hanna <bjhanna728@gmail.com>
2019-04-02 22:42:04 -0400

    add in HOC and hook into default store for saving/displaying of primary category

--- src/components/PrimaryTermSelect.js
+++ src/components/PrimaryTermSelect.js
@@ -1,56 +1,110 @@
+import { Fragment, Component } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
+import { addQueryArgs } from '@wordpress/url';
 import { SelectControl } from '@wordpress/components';
-import { Placeholder, Spinner } from '@wordpress/components';
-import { Fragment } from '@wordpress/element';
 
-const { withSelect } = wp.data;
+const { withSelect, withDispatch } = wp.data;
 
-const mapSelectToProps = (select, props) => {
-    const { taxonomy = 'category' } = props;
-    const { getEntityRecords } = select( 'core' );
-    const { isResolving } = select( 'core/data' );
-    const selectedTermIds = select('core/editor').getCurrentPostAttribute('categories');
-    const query = { 
-        per_page: -1, 
-        hide_empty: false,
-        include: selectedTermIds
-    };
-
-    const primaryTermId = select('core/editor').getCurrentPostAttribute('meta')[`_bjh_primary_${taxonomy}`] || null;
-
-    return {
-        primaryTermId,
-        taxonomy,
-        terms: getEntityRecords( 'taxonomy', taxonomy, query ),
-        isRequesting: isResolving( 'core', 'getEntityRecords', [ 'taxonomy', taxonomy, query ] ),
-    };
-}
-
-
-const Select = ( { isRequesting, terms, primaryTermId = null, taxonomy } ) => {
-
-    if ( isRequesting ) {
-        return (
-            <Fragment>
-                <Placeholder>
-                    <Spinner />
-                </Placeholder>
-            </Fragment>
-        );
-    }
-    
-    const options = terms.map(termObj => ({ label: termObj.name, value: termObj.id }));
-
-    return (
-        <SelectControl
-            label={ `Primary ${taxonomy}` }
-            noOptionLabel="Select a Term"
-            onChange={ val => console.log(val) }
-            value={ primaryTermId }
-            options={ options }
-        />
-    );
+const DEFAULT_QUERY = {
+	per_page: -1,
+	orderby: 'name',
+	order: 'asc',
+	_fields: 'id,name,parent',
 };
 
-const PrimaryCategorySelect = withSelect( mapSelectToProps )( Select );
+var mapSelectToProps = function( select ) {
+    return {
+        metaFieldValue: select('core/editor')
+            .getEditedPostAttribute('meta')
+            ['_bjh_primary_category'],
+    }
+};
 
-export default PrimaryCategorySelect;
+var mapDispatchToProps = function( dispatch ) {
+    return {
+        setMetaFieldValue: function( value ) {
+            dispatch( 'core/editor' ).editPost(
+                { meta: { _bjh_primary_category: value } }
+            );
+        }
+    }
+}
+
+const withPrimaryTermSelect = (WrappedComponent, taxonomy) => {
+    return class HierarchicalTermSelectorWithPrimary extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                loading: true,
+                terms: [],
+                availableTerms: [],
+                primaryTermId: null
+            };
+            this.onUpdateTerms = this.onUpdateTerms.bind(this);
+        }
+
+        componentDidMount() {
+            this.fetchTerms();
+        }
+
+        fetchTerms() {
+            const { taxonomy } = this.props;
+            if ( ! taxonomy ) {
+                return;
+            }
+            apiFetch( {
+                path: addQueryArgs( `/wp-json/wp/v2/${ taxonomy.rest_base }`, DEFAULT_QUERY ),
+            } ).then(
+                ( terms ) => {
+                    this.setState( {
+                        loading: false,
+                        availableTerms: terms,
+                    } );
+                },
+                ( xhr ) => { // reject
+                    if ( xhr.statusText === 'abort' ) {
+                        return;
+                    }
+                    this.setState( {
+                        loading: false,
+                    } );
+                }
+            );
+        }
+
+        onUpdateTerms(...args) {
+            this.setState({ terms: args[0] });
+            this.props.onUpdateTerms(...args);
+        }
+
+        render() {
+            const { terms, taxonomy, metaFieldValue } = this.props;
+            const { availableTerms } = this.state;
+            const options = availableTerms
+                .filter(term => terms.includes(term.id))
+                .map(term => ({ label: term.name, value: term.id }));
+
+            return (
+                <Fragment>
+                    <WrappedComponent {...this.props} onUpdateTerms={this.onUpdateTerms} />
+                    <SelectControl
+                        label={ `Primary ${taxonomy.labels.singular_name}` }
+                        noOptionLabel="Select a Term"
+                        onChange={ this.props.setMetaFieldValue }
+                        value={ metaFieldValue }
+                        options={ options }
+                    />
+                </Fragment>
+            )
+        }
+    }
+}
+
+const addPrimaryTermSelect = (OriginalComponent, props) => {
+    const HierarchicalTermSelectorWithPrimary = withPrimaryTermSelect(OriginalComponent);
+    var HierarchicalTermSelectorWithPrimaryAndData = withSelect( mapSelectToProps )( HierarchicalTermSelectorWithPrimary );
+    var HierarchicalTermSelectorWithPrimaryAndDataAndActions = withDispatch( mapDispatchToProps )( HierarchicalTermSelectorWithPrimaryAndData );
+    return <HierarchicalTermSelectorWithPrimaryAndDataAndActions {...props} />;
+}
+
+export default addPrimaryTermSelect;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import FilterTaxonomyTermSelector from './filter-taxonomy-term-select'; 
-import PrimaryTermSelect from './components/PrimaryTermSelect';
-import { Fragment } from '@wordpress/element';
+import addPrimaryTermSelect from './components/PrimaryTermSelect';
 
 (function ( hooks, { taxonomies } ) {
 
@@ -9,20 +7,10 @@ import { Fragment } from '@wordpress/element';
         'bjh/primary-term-select',
         (HierarchicalTermSelector) => {
             return (props) => {
-                let terms;
-                console.log(props);
                 if (taxonomies.indexOf(props.slug) === -1) {
-                    return (
-                        <HierarchicalTermSelector { ...props } />
-                    )
+                    return <HierarchicalTermSelector {...props} />;
                 }
-                return ( 
-                    <Fragment>
-                        Put that fucking component here
-                        <HierarchicalTermSelector onUpdateTerms={obj => terms = obj.terms} {...props} />
-                        <PrimaryTermSelect taxonomy={props.slug} terms={terms} />
-                    </Fragment>
-                )
+                return addPrimaryTermSelect(HierarchicalTermSelector, props);
             }
         }
     );
